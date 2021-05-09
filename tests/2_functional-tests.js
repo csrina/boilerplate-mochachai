43cb94e33930c6cf7d1dece40aa378c86d4a1bda
Virina Mekhail <virnamekhail@hotmail.com>
2021-05-09 14:41:35 -0600

    fixing functional - tests 5, 6

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -75,7 +75,7 @@ suite("Functional Tests with Zombie.js", function () {
         browser.fill("surname", "Colombo").pressButton("submit", function () {
             browser.assert.success();
             browser.assert.text('span#name', 'Cristoforo');
-            browser.assert.text('span#surname ', 'Colombo');
+            browser.assert.text('span#surname', 'Colombo');
             browser.assert.element('span#dates', 1);
             done();
             });
@@ -86,7 +86,7 @@ suite("Functional Tests with Zombie.js", function () {
        browser.fill("surname", "Vespucci").pressButton("submit", function () {
         browser.assert.success();
         browser.assert.text('span#name', 'Amerigo');
-        browser.assert.text('span#surname ', 'Vespucci');
+        browser.assert.text('span#surname', 'Vespucci');
         browser.assert.element('span#dates', 1);
         done();
        })
